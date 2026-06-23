ec025d126bb19e122af239ef986611ca73258e8f
Pivinha021 <lucaspivaa2009@gmail.com>
2026-06-22 21:30:12 -0300

    sla

--- backend/tests/api.test.js
+++ backend/tests/api.test.js
@@ -38,6 +38,7 @@ test("criar um novo jogo", async () => {
 });
 
 //POST com retorno de erros
+
 test("post com retorno de erro", async () => {
     const response = await request(app).post("/api/games").send({});
     expect(response.statusCode).toBe(500);
@@ -71,3 +72,59 @@ test("Buscar um jogo por id", async () => {
 
     expect(respone2.statusCode).toBe(200);
 });
+
+test("exemplo", async() => {
+    
+    const Criarresponse = await request(app).post("/api/games")
+    .send({
+        title: "Altos pode crê",
+        genre: "Ação",
+        release_year: 2026
+    });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.title).toBe("Altos pode crê");
+    expect(response.body.genre).toBe("Ação");
+    expect(response.body.release_year).toBe(2026);
+
+    const getresponse = await request(app).get("/api/games/1");
+    expect(response.statusCode).toBe(200);
+
+    const Deleteresponse = await request(app).delete(`api/games/1`)
+
+    const UltimoGetresponse = await request(app).get("/api/games/1");
+    expect(response.statusCode).toBe(200);
+});
+
+// Fluxo completo
+
+// Criar jogo
+
+const createResponse = await request(app)
+    .post("/api/games")
+    .send({
+        title: "Minecraft"
+});
+
+const id = createResponse.body.id;
+
+// Buscar o jogo
+
+const getResponse = await request(app)
+    .get(`/api/games/${id}`);
+
+expect(getResponse.statusCode).toBe(200);
+
+// Deletar
+
+const deleteResponse = await request(app)
+    .delete(`/api/games/${id}`);
+
+expect(deleteResponse.statusCode).toBe(200);
+
+// Buscar novamente
+
+const getDeletedResponse = await request(app)
+    .get(`/api/games/${id}`);
+
+expect(getDeletedResponse.statusCode).toBe(404);
